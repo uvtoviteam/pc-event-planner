5465b80625f2d1feabf85bc01093fddf6dad9ddb
Tryxavier <elber.da10@e-uvt.ro>
2022-05-08 14:26:15 +0300

    View_Event

--- src/main/java/com/example/eventplanner/MainMenu.java
+++ src/main/java/com/example/eventplanner/MainMenu.java
@@ -1,25 +1,74 @@
 package com.example.eventplanner;
 
+import genericclasses.Event;
+import genericclasses.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.w3c.dom.events.MouseEvent;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainMenu {
     @FXML
-    Button LogoutButton,CalendarButton,NotifButton,SettingsButton,CreateEventButton,ViewEventButton,RefreshButton,EventManagementButton;
+    Button TestEventButton, LogoutButton,CalendarButton,NotifButton,SettingsButton,CreateEventButton,ViewEventButton,RefreshButton,EventManagementButton;
 
     @FXML
     TextField SearchField;
 
     @FXML
     Text NotifText;
+
+    @FXML
+    TableView<EventModel> eventTableView = new TableView<>();
+
+    @FXML
+    public TableColumn<Event, Integer> eventID;
+    @FXML
+    public TableColumn<Event,String> eventName;
+
+    @FXML
+    public TableColumn<Event, Integer> participantsNum;
+
+    @FXML
+    public TableColumn<Event, LocalDateTime> startDate;
+
+    @FXML
+    public TableColumn<Event, LocalDateTime> endDate;
+
+    List<Event> eventList=new ArrayList<>();
+
+    ArrayList<User> userList=new ArrayList<>();
+    private void addUser(){
+        userList.add(new User(2, "Ion", "12345678", "ion.gmail.com"));
+    }
+
+
+    private ObservableList<EventModel> eventModels = FXCollections.observableArrayList(
+            new EventModel( 1," SLjazzing","Este vorba despre o plimbare muzicala cu Tramvaiul Turistic ce strabate orasul de pe Bega, totul pe acorduri Jazzy, așa cum v-am obișnuit.",LocalDateTime.now(),LocalDateTime.now(),new ArrayList<User>(), 50)
+            );
+    @FXML
+    protected void onTestEvent(){
+        addUser();
+        eventModels.add(new EventModel( 2,"Street Food Festival","Dacă îți dorești un prânz/ o cină în aer liber, sau doar vrei să ieși la o băutură rece alături de prieteni, la Iulius Town vei putea face asta, iar noi te așteptăm cu brațele deschise!",LocalDateTime.now(),LocalDateTime.now(),userList,50));
+      eventID.setCellValueFactory(new PropertyValueFactory<>("ID"));  //GETTER NAME
+      eventName.setCellValueFactory(new PropertyValueFactory<>("Nume"));
+      participantsNum.setCellValueFactory(new PropertyValueFactory<>("Limit"));
+      startDate.setCellValueFactory(new PropertyValueFactory<>("Startdate"));
+      endDate.setCellValueFactory(new PropertyValueFactory<>("Enddate"));
+      eventTableView.setItems(eventModels);
+    }
 
     @FXML
     ImageView logo;
@@ -60,7 +109,22 @@ public class MainMenu {
     }
     @FXML
     protected void onEventManagementButtonClick(){
-
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginController.class.getResource("event-manager-view.fxml"));
+        Scene scene = null;
+        try {
+            Stage stage= new Stage();
+            scene = new Scene(fxmlLoader.load());
+            stage.setMinWidth(304);
+            stage.setMinHeight(262);
+            stage.setTitle("Event Management");
+            stage.setScene(scene);
+            //stage.setResizable(false);
+            stage.show();
+            Stage stagelogin= (Stage) EventManagementButton.getScene().getWindow();
+            stagelogin.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
     @FXML
     protected void onSettingsButtonClick(){
@@ -70,4 +134,37 @@ public class MainMenu {
     protected void onCalendarButtonClick(){
 
     }
+
+    @FXML
+    protected void onMouseClickTable(){
+        //eventTableView.getSelectionModel().getSelectedItem();
+        EventModel eventSelected = eventTableView.getSelectionModel().getSelectedItem();
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginController.class.getResource("View_Event.fxml"));
+        Scene scene = null;
+        if (eventSelected == null) System.out.println("couldn't find event");
+        else {
+
+
+            try {
+                Stage stage = new Stage();
+                scene = new Scene(fxmlLoader.load());
+                stage.setMinWidth(304);
+                stage.setMinHeight(262);
+                stage.setTitle("View Event");
+                stage.setScene(scene);
+                //stage.setResizable(false);
+                ViewEventController controller = fxmlLoader.<ViewEventController>getController();
+                controller.initialize(eventSelected);
+                stage.show();
+
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+
+
+    }
+
 }
